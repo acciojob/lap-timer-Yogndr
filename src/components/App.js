d2a443cd95bf63c5dea6060848615e4843de40f1
Yogender <yashmittal92002@gmail.com>
2025-08-25 09:20:36 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 
-import React from "react";
+import React, { useRef, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+ 
+  
   return (
     <div>
+
+      <button>Start</button>
+      <button>Stop</button>
+      <button>Lap</button>
+      <button>Reset</button>
         {/* Do not remove the main div */}
     </div>
   )
